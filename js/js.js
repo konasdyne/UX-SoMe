fbd8fefc081f9c8ce0ff46658e0ccd1d2ec947af
Anna Vien <eaaavi@students.eaaa.dk>
2019-04-05 13:37:02 +0200

    uddannelsesbar

--- js/js.js
+++ js/js.js
@@ -5,21 +5,3 @@
 console.log('Stardate 235678.89: JavaScript from js/js.js is up and running.');
 
 
-/*!
- * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
- * Copyright 2017.
- * Licensed under the MIT license
- */
-$( document ).ready( function () {
-    $( '.navbar a.dropdown-toggle' ).on( 'click', function ( e ) {
-        var $el = $( this );
-        var $parent = $( this ).offsetParent( ".dropdown-menu" );
-        $( this ).parent( "li" ).toggleClass( 'show' );
-
-        if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
-            $el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
-        }
-        $( '.navbar-nav li.show' ).not( $( this ).parents( "li" ) ).removeClass( "show" );
-        return false;
-    } );
-} );
