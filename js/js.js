3fc78592e8c082184f0f06642ff10530aaa3ad3f
Signe <battrup@live.com>
2019-04-10 13:21:25 +0200

    .

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 
 
 // ÅBNINGSTIDER PÅ FORSIDEN
-
+/*
 var today = new Date();
 var hourNow = today.getHours();
 var open;
@@ -18,7 +18,27 @@ if (hourNow < 8 ) {
 now.innerHTML = open;
 
 
+*/
 
+
+
+var today = new Date();
+var hourNow = today.getHours();
+var open;
+
+if (hourNow < 8 ) {
+    open = '<div class="roed"></div>';
+    console.log(open);
+} else if (hourNow > 8 ) {
+    open = '<div class="groen"></div>';
+    console.log(open);
+}
+
+
+
+if(openNow){
+    openNow.innerHTML = open;    
+}
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic29maWVncm9uYmVyZyIsImEiOiJjanRpYnFmMGMxM3o0M3lwZHNrcXhvazl2In0.zvZxr4rV4hziAJdG6ZM79A';
 
@@ -52,7 +72,7 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/sofiegronberg/cjtibvkd50xqk1fk2slb6v0wx',
     center: [10.154678, 56.118114],
-    zoom: 15.5
+    zoom: 14.5
 });
 
 // add markers to map
